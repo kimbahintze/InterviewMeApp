d4a92eb0a7082fc5ae4c30c1388b3b43f033214b
Kevin <kevinw5@live.com>
2018-05-16 15:54:45 -0600

    Change to storyboard

--- InterviewMeApp/InterviewMeApp/View Controllers/ChatRoomController.swift
+++ InterviewMeApp/InterviewMeApp/View Controllers/ChatRoomController.swift
@@ -14,6 +14,8 @@ class ChatRoomController {
     
     static let shared = ChatRoomController()
     
+    private let jobIndustry = "softwaredevelopmentit"
+    
     enum NotificationKeys {
         static let reloadTable = Notification.Name("ReloadTable")
     }
@@ -28,15 +30,29 @@ class ChatRoomController {
     }
     
     func enterLobby() {
-        
+        Database.database().reference().child("jobIndustry/\(jobIndustry)/videoChatRooms/\(user?.uid ?? "")").setValue(["name": user?.displayName ?? ""])
     }
     
     func fetchLobby() {
+        if chatLobbyUsers.count > 0 {
+            chatLobbyUsers.removeAll()
+        }
         
+        Database.database().reference().child("jobIndustry/\(jobIndustry)/videoChatRooms").observe(.value) { (snapshot) in
+            if let dictionary = snapshot.value as? [String:Any] {
+                var fetchedLobbyUsers: [String] = []
+                dictionary.forEach({ (key, value) in
+                    guard let userDictionary = value as? [String:Any] else { return }
+                    guard let user = userDictionary["name"] as? String else { return }
+                    fetchedLobbyUsers.append(user)
+                })
+                self.chatLobbyUsers = fetchedLobbyUsers
+            }
+        }
     }
     
     func leaveLobby() {
-        
+        Database.database().reference().child("jobIndustry/\(jobIndustry)/videoChatRooms/\(user?.uid ?? "")").setValue(["name": user?.displayName ?? ""])
     }
     
     private init() {
--- InterviewMeApp/InterviewMeApp/View Controllers/ChatRoomViewController.swift
+++ InterviewMeApp/InterviewMeApp/View Controllers/ChatRoomViewController.swift
@@ -11,12 +11,36 @@ import TwilioVideo
 
 class ChatRoomViewController: UIViewController {
     
+    let baseTokenURL = "https://wisteria-saola-1695.twil.io/create-video-token"
+    
+    var camera: TVICameraCapturer?
+    var localVideoTrack: TVILocalVideoTrack?
+    var localAudioTrack = TVILocalAudioTrack()
+    
     @IBOutlet weak var previewView: TVIVideoView!
     
     override func viewDidLoad() {
         super .viewDidLoad()
-        
+        ChatRoomController.shared.enterLobby()
+        startPreview()
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super .viewDidDisappear(animated)
+        ChatRoomController.shared.leaveLobby()
+    }
     
+    private func startPreview() {
+        camera = TVICameraCapturer(source: .frontCamera, delegate: self)
+        previewView.contentMode = .scaleAspectFit
+        localVideoTrack = TVILocalVideoTrack(capturer: camera!)
+        localVideoTrack?.addRenderer(previewView)
+    }
+}
+
+extension ChatRoomViewController: TVICameraCapturerDelegate {
+    
+    func cameraCapturer(_ capturer: TVICameraCapturer, didStartWith source: TVICameraCaptureSource) {
+        previewView.shouldMirror = (source == .frontCamera)
+    }
 }
