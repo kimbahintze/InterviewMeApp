cc674b62248be50237e8999fa0826a982b6b2059
Kim Lundquist <kimbahintze@gmail.com>
2018-05-22 17:45:24 -0600

    tuesday

--- InterviewMeApp/InterviewMeApp/Resources/Constants.swift
+++ InterviewMeApp/InterviewMeApp/Resources/Constants.swift
@@ -12,6 +12,10 @@ import UIKit
 let mainColor = UIColor(red: 44/255, green: 212/255, blue: 140/255, alpha: 1.0)
 let lightFontColor = UIColor(red: 169/255, green: 170/255, blue: 198/255, alpha: 1.0)
 let darkFontColor = UIColor(red: 100/255, green: 100/255, blue: 118/255, alpha: 1.0)
+let GTWalsheimRegular = "GTWalsheimRegular"
+let GTWalsheimMedium = "GTWalsheimMedium"
+let GTWalsheimBold = "GTWalsheimBold"
+
 
 extension String {
     
