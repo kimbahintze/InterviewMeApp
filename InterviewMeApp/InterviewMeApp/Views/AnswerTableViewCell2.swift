adcecf7818e7f8362ab2de791044457192f8a63d
Kevin <kevinw5@live.com>
2018-05-23 10:56:28 -0600

    interview question enhancement

---
+++ InterviewMeApp/InterviewMeApp/Views/AnswerTableViewCell2.swift
@@ -0,0 +1,13 @@
+//
+//  AnswerTableViewCell2.swift
+//  InterviewMeApp
+//
+//  Created by Kevin Wood on 5/23/18.
+//  Copyright © 2018 Kim Lundquist. All rights reserved.
+//
+
+import UIKit
+
+class AnswerTableViewCell: UITableViewCell {
+    
+}
